5b0ce73c4dd5cadbb63cbc576bfdc65e1d9dc13a
Bhagyashree Shivaji Kale <134477227+BhagyashreeKale@users.noreply.github.com>
2024-01-16 17:46:22 +0530

    Inheritance

--- myproject/lib/list1.dart
+++ myproject/lib/list1.dart
@@ -1,49 +1,3 @@
-// main(List <String> args){
-//     List <int > numbers= new List();
-//     numbers.add(0);
-//     numbers.add(2);
-//     numbers.add(4);
-//     numbers.add(6);
-//     numbers.add(8);
-//     for (var i=0;i<numbers.length;i++){
-//         print("Position $i : ${numbers[i]}");
-//     }
-// }
-// 
-
-// class Animal{
-//   String color="Blue";
-//   Animal(this.color){
-    
-//     print("Animal class consructor called");
-//     }
-
-//   Animal. myNamedconstructor(String color){
-//     print("Animal named constructor");
-//   }
-// }
-
-//   class Dog extends Animal{
-//     String? breed;
-//     Dog(this.breed,super.color){
-      
-//       print("Dog class constructor called");
-//   }
-//     Dog.myNamedconstructor(String color,String breed):super.myNamedconstructor(color){
-//       print("Dog Named constructor");
-//     }
-//   }
-//   void main(){
-//     var dog1=Dog("Black","Labrador");
-//     print(" ");
-//     var dog2=Dog ("Brown","Pug");
-//     print(" ");
-//     var dog3= dog.myNamedconstructor("German");
-
-
-//   }
-
-
 //Inheritance
 
 void main(){
@@ -58,11 +12,8 @@ abstract class Shape{
   String? y;
 
   void draw();
-
-
-  void display(){
-
-  }
+  
+   void display();
 
 }
 class Rectangle extends Shape{

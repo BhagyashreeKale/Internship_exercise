5a69e0a01d9604e417443cbe83155f3164de090c
BhagyashreeKale <bhagyshreekale10.com>
2024-01-16 12:34:15 +0530

    Arrow function

---
+++ myproject/lib/function.dart
@@ -0,0 +1,18 @@
+void main(){
+  print("Hello world");
+
+  String name="GOOD";
+  num age=23;
+  num height=5.9;
+  bool isMarried=true;
+
+  print("Name is $name");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+
+ multiply(10,50);
+
+}
+
+  void multiply(int a, int b)=> print(a*b);

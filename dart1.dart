63c7649ce542da7187f3a7dd5cc1a61d0cfc1790
Bhagyashree Shivaji Kale <134477227+BhagyashreeKale@users.noreply.github.com>
2024-01-16 11:21:38 +0530

    Create dart1.dart

---
+++ dart1.dart
@@ -0,0 +1,13 @@
+//operations 
+int n1=10;
+int n2=5;
+
+int sum=n1+n2;
+int sub=n1-n2;
+int mul=n1*n2;
+double div=n1/n2 ;
+
+print("The sum is $sum");
+print("The sub is $sub");
+print("The mul is $mul");
+print("The div is $div");

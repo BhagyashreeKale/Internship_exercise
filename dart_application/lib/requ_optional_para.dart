31f92f1e91e2e8eb6cc0fc04641d6dfbed1c7bb9
BhagyashreeKale <bhagyshreekale10.com>
2024-01-17 11:59:05 +0530

    Required and opional Positional Parameters

---
+++ dart_application/lib/requ_optional_para.dart
@@ -0,0 +1,21 @@
+//Required pararmeters
+void main(){
+  printcities("New York","New Delhi","Sydney");
+  
+  print(" ");
+
+  printcountries("USA","India","Australia");
+}
+
+
+void printcities(String name1, String name2,String name3){
+  print("Name1 is $name1");
+  print("Name2 is $name2");
+  print("Name3 is $name3");
+}
+//optional positional pararmeters
+void printcountries(String name1, String name2,String name3){
+  print("Name1 is $name1");
+  print("Name2 is $name2");
+  print("Name3 is $name3");
+}
